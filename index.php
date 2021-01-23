0c465d79a54008d6be9a34abca0a690239b17de4
Nelchel <elleanore25@gmail.com>
2021-01-23 16:25:32 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+//session_destroy();
+include "db.php";
+
+include "createMusique.php";
+include "playliste.php";  
+include "readMusique.php";
+include "updateMusique.php";
+include "createMembre.php";
+include "membres.php";  
+include "boutique.php";
+include "action.php";
+include "view.php";
