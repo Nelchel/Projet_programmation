bb1c8f1137669c1078df4da97be60141f508b0b4
Nelchel <elleanore25@gmail.com>
2021-02-03 14:44:57 +0100

    teste2

--- index.php
+++ index.php
@@ -1,15 +1,2 @@
-<?php
 
-session_start();
-//session_destroy();
-include "db.php";
-
-include "createMusique.php";
-include "playliste.php";  
-include "readMusique.php";
-include "updateMusique.php";
-include "createMembre.php";
-include "membres.php";  
-include "boutique.php";
-include "action.php";
-include "view.php";
+dfligjndfkfjn bfgkjn sdmfldz;fld,;g,;ddfgvldngvkljdfnv
